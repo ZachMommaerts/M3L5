fff29efd1236a756c1d1413dbdd0be0982a244fd
ZachMommaerts <zmommaerts@gmail.com>
2023-07-15 16:59:01 -0500

    Initial Commit

---
+++ M3L5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M3L5
+//
+//  Created by Zach Mommaerts on 7/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3L5/M3L5App.swift
@@ -0,0 +1,17 @@
+//
+//  M3L5App.swift
+//  M3L5
+//
+//  Created by Zach Mommaerts on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct M3L5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
